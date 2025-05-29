b5b160aa86f55198133153efe88bd2c9fb48e00a
ckechios <62009820+ckechios@users.noreply.github.com>
2025-05-29 18:33:12 +0000

    added commonmodule and model

--- my-app/src/app/app.ts
+++ my-app/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Items } from './components/items/items';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Items],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- my-app/src/app/components/items/items.ts
+++ my-app/src/app/components/items/items.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { Itemsmod } from '../../models/itemsmod';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-items',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './items.html',
   styleUrl: './items.css'
 })
 export class Items {
+  items_title = "Items List"
 
+  items: Itemsmod[] = [];
+
+  ngOnInit() {
+    this.items = [
+      { id: 1, desc: 'Item 1'},
+      { id: 2, desc: 'Item 2'}
+    ]
+  }
 }
